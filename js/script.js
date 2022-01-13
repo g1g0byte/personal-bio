e352f8a7b8213a7984b750a9ad62a5d6c4344a43
Gabriel MacCallum <73905643+g1g0byte@users.noreply.github.com>
2022-01-13 19:40:20 +0000

    toggle gradient + toggle dark/light theme

--- js/script.js
+++ js/script.js
@@ -26,14 +26,14 @@ function hideNavBar() {
     navbar_items_wrapper.className = "navbar__Items"
 }
 
-const navbar_items = document.querySelectorAll('.navbar__Items')
+const navbar_items = document.querySelectorAll(".navbar__Items")
 
 function toggleMobileNav() {
-    navbar_items.forEach(navbar_items => navbar_items.classList.toggle('navbar__ToggleShow'));
+    navbar_items.forEach(navbar_items => navbar_items.classList.toggle("navbar__ToggleShow"));
 }
 
-document.querySelector('.navbar__Link-toggle')
-    .addEventListener('click', () => {
+document.querySelector(".navbar__Link-toggle")
+    .addEventListener("click", () => {
         toggleMobileNav();
     });
 
@@ -46,3 +46,41 @@ window.addEventListener("scroll", function () {
         scroll_up_arrow.style.display = "block";
     }
 }, false);
+
+
+const title = document.getElementById("title");
+
+function toggleGradient() {
+    if (title.classList.contains("gradientAnimation")) {
+        title.classList.remove("gradientAnimation")
+    }
+    else {
+        title.classList.add("gradientAnimation")
+    }
+}
+
+// Theme switching //
+const toggleSwitch = document.getElementById("theme-switch-checkbox");
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
